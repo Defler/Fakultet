40a2c09bf4589299d135be65eab1cd5be475992f
PatrykRed <patryk.redykiert@gmail.com>
2021-05-10 17:32:37 +0200

    ćw 2

--- aplikacja/src/App.tsx
+++ aplikacja/src/App.tsx
@@ -1,47 +1,23 @@
 import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
+import Home from './views/home/Home';
+import Movie from './views/movie/Movie';
+import Page404 from './views/page404/Page404'; 
+
 import './App.css';
 
-import CustomHeader, { addNumber } from './components/CustomHeader';
-import Counter from './components/Counter';
-
-
 const App = () => {
-    const addedNumber = addNumber(1, 2);
-    const [changedNumber, setChangedNumber] = React.useState(0);
-
-    const handleNumberChange = (newNumber: number) => {
-        setChangedNumber(newNumber);
-    }
-
-    const liczbaMniejszaOdZera = (liczba: number) => {
-        if (liczba < -10) {
-            return (<div>Liczba jest mniejsza od -10 </div>)
-        }
-        else if (liczba < 0) {
-            return (<div>Liczba jest mniejsza od zera </div>)
-        }
-    }
-
-    const liczbaWiekszaOdZero = (liczba: number) => {
-        if (liczba > 15) {
-            return (<div>Liczba przekroczona </div>)
-        }
-        else if (liczba > 10) {
-            return (<div>Liczba jest wieksza od dziesiec </div>)
-        }
-        else if (liczba > 0) {
-            return (<div>Liczba jest wieksza od zero </div>)
-        }
-        
-    }
-
+    
     return (
         <div className="App">
-            <CustomHeader>
-                <Counter onNumberChange={handleNumberChange} />
-                {liczbaMniejszaOdZera(changedNumber)}
-                {liczbaWiekszaOdZero(changedNumber)}
-            </CustomHeader>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/movie/:id" component={Movie} />
+                    <Route path="/" component={Home} exact />
+                    <Route path="*" component={Page404} />
+                </Switch>
+            </BrowserRouter>
         </div>
     );
 }
